c28cf3c2aa13696e213f7769680eae306bfe4045
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2021-04-14 22:32:05 +0200

    First commit

--- src/ResNet9.jl
+++ src/ResNet9.jl
@@ -1,5 +1,40 @@
 module ResNet9
 
-# Write your package code here.
+using Flux
+using Flux: convfilter, Zeros
+
+export resnet9
+
+function convblock(f::Pair; pool = false)
+    _, out = f
+    w = convfilter((3,3), f)
+    b = Zeros()
+    cv = Conv(w, b, identity, pad = (1,1))
+    bn =  BatchNorm(out, relu)
+    pool && return cv, bn, MaxPool((2, 2))
+    cv, bn
+end
+
+convblock(f::Int) = convblock(f=>f)
+
+resblock(f) = SkipConnection(Chain(convblock(f)..., convblock(f)...), +)
+
+function classifier(f, n)
+    fl = (GlobalMaxPool(), Flux.flatten)
+    n > 2 && return fl..., Dense(f, n), softmax
+    fl..., Dense(f, 1, sigmoid)
+end
+
+function resnet9(;ichs, ncls, flts = 64) # dropout, groupnorm, bson file resnet9!
+    f1 = flts
+    f2 = 2 * f1
+    f3 = 2 * f2
+    f4 = 2 * f3
+    Chain(convblock(ichs=>f1)...,                          # Input layer
+          convblock(f1=>f2, pool = true)..., resblock(f2), # Layer 1
+          convblock(f2=>f3, pool = true)...,               # Layer 2
+          convblock(f3=>f4, pool = true)..., resblock(f4), # Layer 3
+          classifier(f4, ncls)...)                         # Classifier
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,27 @@
 using ResNet9
 using Test
+using Flux
+using CUDA
+
+if CUDA.has_cuda_gpu()
+    #CUDA.allowscalar(false)
+    device = gpu
+else
+    device = cpu
+end
 
 @testset "ResNet9.jl" begin
-    # Write your tests here.
+    @testset "Two classes" begin
+        model = resnet9(ichs = 3, ncls = 2) |> device
+        x = Float32.(rand(32, 32, 3, 4)) |> device
+        ŷ = model(x)
+        @test size(ŷ) == (1, 4)
+    end
+
+    @testset "Five classes" begin
+        model = resnet9(ichs = 3, ncls = 5) |> device
+        x = Float32.(rand(32, 32, 3, 4)) |> device
+        ŷ = model(x)
+        @test size(ŷ) == (5, 4)
+    end
 end
