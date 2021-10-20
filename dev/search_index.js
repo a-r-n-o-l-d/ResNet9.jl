7d96949a5deea893734a85f90330f7ea357ff542
Documenter.jl <documenter@juliadocs.github.io>
2021-10-20 14:54:22 +0000

    build based on 39daec5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ResNet9","category":"page"},{"location":"#ResNet9","page":"Home","title":"ResNet9","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ResNet9.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ResNet9]","category":"page"},{"location":"#ResNet9.resnet9-Tuple{}","page":"Home","title":"ResNet9.resnet9","text":"resnet9(;inchannels, nout, basewidth = 64)\n\n\n\n\n\n","category":"method"}]
+}
