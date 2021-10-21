cf0b7c45fc4e0fd922f95b4371c4d540d45cc3a3
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2021-10-21 17:32:26 +0200

    transfer learning utilities

--- src/ResNet9.jl
+++ src/ResNet9.jl
@@ -6,10 +6,12 @@ using Base.Iterators: takewhile
 
 export resnet9
 
+conv(chs) = Conv((3, 3), chs, identity, pad = (1, 1), bias = false)
+
 function convblock(chs::Pair; pool = false)
     _, out = chs
-    cv = Conv((3, 3), chs, identity, pad = (1, 1), bias = false)
-    bn =  BatchNorm(out, relu)
+    cv = conv(chs)
+    bn = BatchNorm(out, relu)
     pool && return cv, bn, MaxPool((2, 2))
     cv, bn
 end
@@ -25,7 +27,6 @@ function classifier(ch, nc, drp)
          push!(fc, Dropout(drp))
     end
     (nc > 2) ? push!(fc, Dense(ch, nc), softmax) : push!(fc, Dense(ch, 1, sigmoid))
-    #fc = (nc > 2) ? [Dense(ch, nc), softmax] : [Dense(ch, 1, sigmoid)]
     fl..., fc...
 end
 
@@ -53,43 +54,110 @@ end
 
 """
     resnet9(model::Chain; nclasses, dropout)
-Build a ResNet9 model from an existing (pre-trained) model and change the last
-layers according to `nclasses` and `dropout`.
+Build a ResNet9 model from an existing (pre-trained) model and change the first 
+or last layers according to `inchannels`, `nclasses` and `dropout`.
 """
-function resnet9(model::Chain; nclasses, dropout)
-    back = takewhile(l -> !isa(l, GlobalMaxPool), model) |> collect
-    ch = nchout(model)
-    front = classifier(ch, nclasses, dropout)
-    Chain(back..., front...)
+function resnet9(model::Chain;
+                 inchannels = inchan(model), 
+                 nclasses = nclass(model),
+                 dropout = dpout(model))
+    ich = inchan(model)
+    if inchannels != ich
+        l = model[1]
+        iout = size(l.weight, ndims(l.weight))
+        model = Chain(conv(inchannels=>iout), model[2:end]...)
+    end
+    if nclasses != nclass(model)
+        fc = fclayer(model)
+        fcin = size(fc.weight, 2)
+        i = (findall(l -> isa(l, GlobalMaxPool), model) |> first) - 1 #takewhile(l -> !isa(l, GlobalMaxPool), model) |> collect
+        back = model[1:i]
+        front = classifier(fcin, nclasses, dropout)
+        model = Chain(back..., front...)
+    elseif dropout != dpout(model)
+        i = (findall(l -> isa(l, GlobalMaxPool), model) |> first) + 1
+        j = findall(l -> isa(l, Dense), model) |> first
+        back = model[1:i]
+        front = model[j:end]
+        front = (dropout > 0) ? [Dropout(dropout), front...] : front
+        model = Chain(back..., front...)
+    end
+    model
 end
 
-"""
-    resnet9(model::Chain; nclasses, dropout)
-Build a ResNet9 model from an existing (pre-trained) model and change the first
-layer according to `inchannels`.
-"""
-function resnet9(model::Chain; inchannels)
-    ch = nchout(model[1:2])
-    Chain(convblock(inchannels=>ch)..., model[3:end]...)
-end
+inchan(m) = size(m[1].weight, ndims(m[1].weight) - 1)
 
-nchout(l) = 0
-nchout(l::Conv) = size(l.weight, ndims(l.weight))
-nchout(l::BatchNorm) = l.chs
-nchout(l::SkipConnection) = nchout(l.layers)
-function nchout(c::Chain)
-    layers = [c...] |> reverse
-    nch = 0
-    for l ∈ layers
-        nch = nchout(l)
-        if nch > 0
-            break
-        end
-    end
-    if nch == 0
-        error("Failed to find number of channels.") |> throw
-    end
-    nch
-end
+fclayer(m) = filter(l -> isa(l, Dense), m.layers) |> first
+
+nclass(m) = fclayer(m) |> l -> size(l.weight, 1)
+
+dpout(m) = m[isa.(m, Dropout)] |> c -> (isempty(c.layers)) ? 0 : c.layers[1].p
+
+# """
+#     resnet9(model::Chain; nclasses, dropout)
+# Build a ResNet9 model from an existing (pre-trained) model and change the first
+# layer according to `inchannels`.
+# """
+# function resnet9(model::Chain; inchannels)
+#     ch = nchout(model[1:2])
+#     Chain(convblock(inchannels=>ch)..., model[3:end]...)
+# end
+
+# function modelarch(model::Chain)
+#     l = model[1]
+#     ich = size(l.weight, ndims(l.weight) - 1)
+#     fcin, fcout = 0, 0
+#     for l ∈ model
+#         if isa(l, Dense)
+#             fcin = size(l.weight, 2)
+#             fcout = size(l.weight, 1)
+#         end
+#     end
+#     ich, fcin, fcout
+# end
+
+# function ncl(m::Chain)
+#     # for l ∈ 
+#     #     takewhile(l -> !isa(l, Dense), m)
+
+#     # size(d.weight, 1)
+# end
+
+# nchin(l) = 0
+# nchin(l::Conv) = size(l.weight, ndims(l.weight) - 1)
+# nchin(l::BatchNorm) = l.chs
+# nchin(l::SkipConnection) = nchin(l.layers)
+# function nchin(c::Chain)
+#     nch = 0
+#     for l ∈ c
+#         nch = nchin(l)
+#         if nch > 0
+#             break
+#         end
+#     end
+#     if nch == 0
+#         error("Failed to find number of channels.") |> throw
+#     end
+#     nch
+# end
+
+# nchout(l) = 0
+# nchout(l::Conv) = size(l.weight, ndims(l.weight))
+# nchout(l::BatchNorm) = l.chs
+# nchout(l::SkipConnection) = nchout(l.layers)
+# function nchout(c::Chain)
+#     layers = [c...] |> reverse
+#     nch = 0
+#     for l ∈ layers
+#         nch = nchout(l)
+#         if nch > 0
+#             break
+#         end
+#     end
+#     if nch == 0
+#         error("Failed to find number of channels.") |> throw
+#     end
+#     nch
+# end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -11,17 +11,32 @@ else
 end
 
 @testset "ResNet9.jl" begin
+    batchsize = 2
+    inchs = 3
+    x1 = Float32.(rand(16, 16, inchs, batchsize)) |> device
+    x2 = Float32.(rand(16, 16, 2 * inchs, batchsize)) |> device
+
     @testset "Two classes" begin
-        model = resnet9(inchannels = 3, nclasses = 1) |> device
-        x = Float32.(rand(32, 32, 3, 4)) |> device
-        ŷ = model(x)
-        @test size(ŷ) == (1, 4)
+        model = resnet9(inchannels = inchs, nclasses = 2) |> device
+        ŷ = model(x1)
+        @test size(ŷ) == (1, batchsize)
     end
 
     @testset "Five classes" begin
-        model = resnet9(inchannels = 3, nclasses = 5) |> device
-        x = Float32.(rand(32, 32, 3, 4)) |> device
-        ŷ = model(x)
-        @test size(ŷ) == (5, 4)
+        model = resnet9(inchannels = inchs, nclasses = 5) |> device
+        ŷ = model(x1)
+        @test size(ŷ) == (5, batchsize)
+    end
+
+    @testset "Transfer learning" begin
+        model = resnet9(inchannels = inchs, nclasses = 5)
+        # Change the number of classes and add dropout
+        model2 = resnet9(model, nclasses = 10, dropout = 0.5) |> device
+        ŷ = model2(x1)
+        @test size(ŷ) == (10, batchsize)
+        # Change the number of input channels
+        model2 = resnet9(model, inchannels = 2 * inchs) |> device
+        ŷ = model2(x2)
+        @test size(ŷ) == (10, batchsize)
     end
 end
